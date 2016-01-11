9882ecadfded964a6972c1f461a2529e808c0d9b
apple <apple@appledeMac.local>
2016-01-11 13:11:56 +0800

    初始化

--- MyTwitter/MyTwitter/Class/Compose/Controller/ZYComposeController.h
+++ MyTwitter/MyTwitter/Class/Compose/Controller/ZYComposeController.h
@@ -1,11 +1,3 @@
-//
-//  ZYComposeController.h
-//  MyTwitter
-//
-//  Created by apple on 15/12/28.
-//  Copyright (c) 2015年 itcast. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ZYComposeController : UIViewController
